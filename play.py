0b950eb2b3c8c9191caf9355dd722eaa11726bf5
Alex Berman <alex@nimations.com>
2014-06-22 10:36:12 +0200

    added remaining sounds

--- play.py
+++ play.py
@@ -3,6 +3,10 @@
 from sequencer import Sequencer
 
 sequencer = Sequencer()
+sequencer.load_sounds("sound/*/*.wav")
+
+
+# Buses
 
 sequencer.add_bus("bubbles")
 sequencer.set_bus_params("bubbles", {
@@ -22,7 +26,9 @@ sequencer.set_bus_params("long_reverb", {
         "reverb_mix": 1.0
         })
 
-sequencer.load_sounds("sound/*/*.wav")
+
+# Water
+
 sequencer.set_params(
     "sound/water/Sound 2 mindre rinn.wav",
     {"send": "long_reverb",
@@ -56,23 +62,62 @@ sequencer.play(
     "sound/water/ZOOM0009c 44100 1.wav",
     looped=1)
 
+
+# Mag
+
 sequencer.set_params(
     "sound/mag/mag_weak*.wav",
     {"send": "long_reverb",
      "gain": 2.1,
-     "comp_threshold": -36.6,
-     "pan": -1})
+     "comp_threshold": -36.6})
+sequencer.set_params(
+    "sound/mag/mag_breath1.wav",
+    {"send": "long_reverb",
+     "gain": 0,
+     "comp_threshold": -36.6})
+
+sequencer.set_params(
+    "sound/mag/*.wav",
+    {"pan": -1})
+
 sequencer.add_group(
-    "sound/mag/mag_weak*.wav")
+    "sound/mag/*.wav")
+
+
+# Hannabiell
 
 sequencer.set_params(
     "sound/hannabiell/tone*.wav",
     {"send": "hannabell_tone_reverb",
      "gain": -100,
      "comp_threshold": -36.6,
-     "send_gain": 12.4,
-     "pan": 1})
+     "send_gain": 12.4})
+
+sequencer.set_params(
+    "sound/hannabiell/breath*.wav",
+    {"send": "long_reverb",
+     "gain": -10.5,
+     "comp_threshold": -30.8})
+
+sequencer.set_params(
+    "sound/hannabiell/fragment*.wav",
+    {"send": "long_reverb",
+     "gain": +11.7})
+
+sequencer.set_params(
+    "sound/hannabiell/squeak*.wav",
+    {"send": "long_reverb",
+     "gain": +0.3})
+
+
+sequencer.set_params(
+    "sound/hannabiell/*.wav",
+    {"pan": 1})
+
 sequencer.add_group(
-    "sound/hannabiell/tone*.wav")
+    "sound/hannabiell/*.wav")
+
+
+# Run everything
 
 sequencer.run_main_loop()
