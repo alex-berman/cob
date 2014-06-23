f70ff398d740fd0666cde3988ce7e2a3445b3402
Alex Berman <alex@nimations.com>
2014-06-23 18:52:44 +0200

    sounds sorted and given more readable lables

--- control_panel.py
+++ control_panel.py
@@ -5,6 +5,7 @@ from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 from client import WebsocketClient
 from event import Event
+import re
 
 class MainWindow(QWidget):
     def __init__(self):
@@ -12,12 +13,19 @@ class MainWindow(QWidget):
 
     def _add_sound_controls(self):
         self._layout = QVBoxLayout()
-        for sound in self._sounds:
+        for sound in sorted(self._sounds):
             self._add_sound_control(sound)
         self.setLayout(self._layout)
 
     def _add_sound_control(self, sound):
-        self._layout.addWidget(QLabel(sound))
+        self._layout.addWidget(QLabel(self._sound_label(sound)))
+
+    def _sound_label(self, filename):
+        matcher = re.match(r"sound\/(.*)\.wav", filename)
+        if matcher:
+            return matcher.group(1)
+        else:
+            return filename
 
     def customEvent(self, custom_qt_event):
         custom_qt_event.callback()
